5070aab194bfa3615dca7c5f437b377d755db388
RobertoJSJTV <Robertojsj18@gmail.com>
2025-10-09 20:04:46 -0300

    modificando para se adequar para telas menores

---
+++ js/conexao.js
@@ -0,0 +1,3 @@
+    const SUPABASE_URL = 'https://buvmqnefhemlmriihyzd.supabase.co';
+    const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1dm1xbmVmaGVtbG1yaWloeXpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4NjgyNjYsImV4cCI6MjA2ODQ0NDI2Nn0._oaLW0WRGwzycasOCCGXeDWnSrWCpmi1pqGr8zfnFqA';
+    const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
